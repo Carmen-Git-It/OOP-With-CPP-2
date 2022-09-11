1bc12ba78139a0412b4ecd0e03cdba817eb4cd2d
Carmen-Git-It <carmenwhitton@gmail.com>
2022-09-11 17:10:09 -0400

    small changes

---
+++ WS01/part 1/carads.cpp
@@ -0,0 +1,12 @@
+// I have done all the coding by myself and only copied the code that
+// my professor provided to complete my workshops and assignments.
+// Name:   Carmen Whitton
+// Email:  cwhitton@myseneca.ca
+// ID#:    102710217
+// Date:   09/11/2022 (Happy birthday to me!)
+
+#include "carads.h"
+
+namespace sdds {
+void listArgs(int argc, char *argv[]) {}
+} // namespace sdds
---
+++ WS01/part 1/carads.h
@@ -0,0 +1,16 @@
+// I have done all the coding by myself and only copied the code that
+// my professor provided to complete my workshops and assignments.
+// Name:   Carmen Whitton
+// Email:  cwhitton@myseneca.ca
+// ID#:    102710217
+// Date:   09/11/2022 (Happy birthday to me!)
+
+#ifndef SDDS_CARADS_H
+#define SDDS_CARADS_H
+
+extern double g_taxrate, g_discount;
+namespace sdds {
+void listArgs(int argc, char *argv[]);
+}
+
+#endif // SDDS_CARADS_H
