35a9a5f0bca10be41ad8c1593e34adc13deaca65
Carmen-Git-It <carmenwhitton@gmail.com>
2022-10-11 19:38:06 -0400

    w5p1 finished; implemented Book module

--- WS05/part 1/Book.h
+++ WS05/part 1/Book.h
@@ -7,7 +7,28 @@
 
 #ifndef SDDS_BOOK_H
 #define SDDS_BOOK_H
+#include <string>
 
-namespace sdds {}
+namespace sdds {
+   class Book
+   {
+      std::string m_author{};
+      std::string m_title{};
+      std::string m_country{};
+      size_t m_year{};
+      double m_price{};
+      std::string m_description{};
+
+    public:
+      Book();
+      Book(const std::string& strBook);
+      const std::string& title() const;
+      const std::string& country() const;
+      const size_t& year() const;
+      double& price();
+      friend std::ostream& operator<<(std::ostream&, const Book&);
+   };
+
+} // namespace sdds
 
 #endif //! SDDS_BOOK_H
--- WS05/part 1/w5_p1.cpp
+++ WS05/part 1/w5_p1.cpp
@@ -3,79 +3,85 @@
 // 2021/01/19 - Chris
 // 2021/02/12 - Cornel
 
-#include <iostream>
-#include <iomanip>
+#include "Book.h"
 #include <fstream>
-#include "Book.h"
-#include "Book.h"
+#include <iomanip>
+#include <iostream>
 
 using namespace sdds;
 
-enum AppErrors
-{
-	CannotOpenFile = 1, // An input file cannot be opened
-	BadArgumentCount = 2, // The application didn't receive anough parameters
+enum AppErrors {
+   CannotOpenFile   = 1, // An input file cannot be opened
+   BadArgumentCount = 2, // The application didn't receive anough parameters
 };
 
 // ws books.txt
-int main(int argc, char** argv)
-{
-	std::cout << "Command Line:\n";
-	std::cout << "--------------------------\n";
-	for (int i = 0; i < argc; i++)
-		std::cout << std::setw(3) << i + 1 << ": " << argv[i] << '\n';
-	std::cout << "--------------------------\n\n";
+int main(int argc, char** argv) {
+   std::cout << "Command Line:\n";
+   std::cout << "--------------------------\n";
+   for (int i = 0; i < argc; i++)
+      std::cout << std::setw(3) << i + 1 << ": " << argv[i] << '\n';
+   std::cout << "--------------------------\n\n";
 
-	// get the books
-	sdds::Book library[7];
-	if (argc == 2) {
-		// TODO: load the collection of books from the file "argv[1]".
-		//       - read one line at a time, and pass it to the Book constructor
-		//       - store each book read into the array "library"
-		//       - lines that start with "#" are considered comments and should be ignored
-		//       - if the file cannot be open, print a message to standard error console and
-		//                exit from application with error code "AppErrors::CannotOpenFile"
-	}
-	else
-	{
-		std::cerr << "ERROR: Incorrect number of arguments.\n";
-		exit(AppErrors::BadArgumentCount);
-	}
+   // get the books
+   sdds::Book library[7];
+   if (argc == 2) {
+      std::ifstream file(argv[1]);
+      if (file) {
+         size_t index{}; // Tracks the number of library indices filled
+         while (file && index < 7) {
+            std::string temp{};
+            std::getline(file, temp, '\n');
+            // If the line isn't a comment
+            if (temp[0] != '#') {
+               library[index++] = Book(temp);
+            }
+         }
+      }
+      else {
+         std::cerr << "Error: Could not open file: " << argv[1] << std::endl;
+         exit(AppErrors::CannotOpenFile);
+      }
+   }
+   else {
+      std::cerr << "ERROR: Incorrect number of arguments.\n";
+      exit(AppErrors::BadArgumentCount);
+   }
 
-	double usdToCadRate = 1.3;
-	double gbpToCadRate = 1.5;
+   double usdToCadRate = 1.3;
+   double gbpToCadRate = 1.5;
 
-	// TODO: create a lambda expression that fixes the price of a book accoding to the rules
-	//       - the expression should receive a single parameter of type "Book&"
-	//       - if the book was published in US, multiply the price with "usdToCadRate"
-	//            and save the new price in the book object
-	//       - if the book was published in UK between 1990 and 1999 (inclussive),
-	//            multiply the price with "gbpToCadRate" and save the new price in the book object
+   // Lambda
+   auto fix = [=](Book& book) {
+      if (!book.country().compare("US")) {
+         book.price() *= usdToCadRate;
+      }
+      else if (!book.country().compare("UK") && book.year() >= 1990 &&
+               book.year() <= 1999) {
+         book.price() *= gbpToCadRate;
+      }
+   };
 
+   std::cout << "-----------------------------------------\n";
+   std::cout << "The library content\n";
+   std::cout << "-----------------------------------------\n";
+   for (Book& b : library) {
+      std::cout << b << std::endl;
+   }
 
+   std::cout << "-----------------------------------------\n\n";
+   for (Book& b : library) {
+      fix(b);
+   }
 
-	std::cout << "-----------------------------------------\n";
-	std::cout << "The library content\n";
-	std::cout << "-----------------------------------------\n";
-	// TODO: iterate over the library and print each book to the screen
+   std::cout << "-----------------------------------------\n";
+   std::cout << "The library content (updated prices)\n";
+   std::cout << "-----------------------------------------\n";
+   for (Book& b : library) {
+      std::cout << b << std::endl;
+   }
 
+   std::cout << "-----------------------------------------\n";
 
-
-	std::cout << "-----------------------------------------\n\n";
-
-	// TODO: iterate over the library and update the price of each book
-	//         using the lambda defined above.
-
-
-
-	std::cout << "-----------------------------------------\n";
-	std::cout << "The library content (updated prices)\n";
-	std::cout << "-----------------------------------------\n";
-	// TODO: iterate over the library and print each book to the screen
-
-
-
-	std::cout << "-----------------------------------------\n";
-
-	return 0;
+   return 0;
 }
