0762846b59e45ab90bcd9e05bc2b8a71f4f88c0a
Carmen-Git-It <carmenwhitton@gmail.com>
2022-10-11 03:39:04 -0400

    Added headers

---
+++ WS05/part 1/Book.h
@@ -0,0 +1,13 @@
+// I have done all the coding by myself and only copied the code that
+// my professor provided to complete my workshops and assignments.
+// Name:   Carmen Whitton
+// Email:  cwhitton@myseneca.ca
+// ID#:    102710217
+// Date:   10/11/2022
+
+#ifndef SDDS_BOOK_H
+#define SDDS_BOOK_H
+
+namespace sdds {}
+
+#endif //! SDDS_BOOK_H
