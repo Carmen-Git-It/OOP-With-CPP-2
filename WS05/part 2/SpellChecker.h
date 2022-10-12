8027b8d086002d679a45fbd45261fbba58a7daeb
Carmen-Git-It <carmenwhitton@gmail.com>
2022-10-11 21:45:11 -0400

    Implemented SpellChecker header

--- WS05/part 2/SpellChecker.h
+++ WS05/part 2/SpellChecker.h
@@ -4,3 +4,23 @@
 // Email:  cwhitton@myseneca.ca
 // ID#:    102710217
 // Date:   10/11/2022
+
+#ifndef SDDS_SPELLCHECKER_H
+#define SDDS_SPELLCHECKER_H
+
+#include <string>
+
+namespace sdds {
+   class SpellChecker
+   {
+      std::string m_badWords[6]{};
+      std::string m_goodWords[6]{};
+
+    public:
+      SpellChecker();
+      SpellChecker(const char* filename);
+      void operator()(std::string& text);
+   };
+} // namespace sdds
+
+#endif //! SDDS_SPELLCHECKER_H
