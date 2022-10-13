0f5e72a988d4c117df1efa849db159d12dab20bf
Carmen-Git-It <carmenwhitton@gmail.com>
2022-10-12 22:17:49 -0400

    Finished Collection.h

--- WS05/part 2/Collection.h
+++ WS05/part 2/Collection.h
@@ -7,10 +7,28 @@
 
 #ifndef SDDS_COLLECTION_H
 #define SDDS_COLLECTION_H
+#include <string>
 
 namespace sdds {
-   class Collection
+   template <typename T> class Collection
    {
+      std::string m_name{};
+      T* m_arr{};
+      size_t m_size{};
+      void (*func)(const Collection<T>&, const T&);
+
+    public:
+      Collection();
+      Collection(const std::string& name);
+      Collection(const Collection&)            = delete;
+      Collection& operator=(const Collection&) = delete;
+      ~Collection();
+      const std::string& name() const;
+      size_t size() const;
+      void setObserver(void (*observer)(const Collection<T>&, const T&));
+      Collection<T>& operator+=(const T& item);
+      T& operator[](size_t idx) const;
+      T* operator[](const std::string& title) const;
    };
 } // namespace sdds
 
