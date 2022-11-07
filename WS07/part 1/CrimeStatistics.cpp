f30219057df6c4e1ad17ed90c0a47737668053ff
Carmen W <Carmenwhitton@gmail.com>
2022-11-06 22:09:29 -0500

    WS07 initial commit

---
+++ WS07/part 1/CrimeStatistics.cpp
@@ -0,0 +1,12 @@
+// I have done all the coding by myself and only copied the code that
+// my professor provided to complete my workshops and assignments.
+// Name:   Carmen Whitton
+// Email:  cwhitton@myseneca.ca
+// ID#:    102710217
+// Date:   11/06/2022
+
+#include "CrimeStatistics.h";
+
+namespace sdds {
+
+}
---
+++ WS07/part 1/CrimeStatistics.h
@@ -0,0 +1,16 @@
+// I have done all the coding by myself and only copied the code that
+// my professor provided to complete my workshops and assignments.
+// Name:   Carmen Whitton
+// Email:  cwhitton@myseneca.ca
+// ID#:    102710217
+// Date:   11/06/2022
+
+#ifndef SDDS_CRIMESTATISTICS_H
+#define SDDS_CRIMESTATISTICS_H
+
+namespace sdds {
+
+}
+
+
+#endif //!SDDS_CRIMESTATISTICS_H
