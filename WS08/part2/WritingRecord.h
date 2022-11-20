c47ece017b14b8435a1149247f2023cbdd1c7082
Carmen W <Carmenwhitton@gmail.com>
2022-11-20 00:38:21 -0500

    Completed part 2

---
+++ WS08/part2/WritingRecord.h
@@ -0,0 +1,21 @@
+// I have done all the coding by myself and only copied the code that
+// my professor provided to complete my workshops and assignments.
+// Name:   Carmen Whitton
+// Email:  cwhitton@myseneca.ca
+// ID#:    102710217
+// Date:   11/18/2022
+
+#ifndef SDDS_WRITINGRECORD_H
+#define SDDS_WRITINGRECORD_H
+
+#include "GeneratingList.h"
+#include "EmpProfile.h"
+
+namespace sdds {
+	GeneratingList<EmployeeWage> writeRaw(const GeneratingList<Employee>& emp, const GeneratingList<Salary>& sal);
+	GeneratingList<EmployeeWage> writeSmart(const GeneratingList<Employee>& emp, const GeneratingList<Salary>& sal);
+}
+
+
+#endif // SDDS_WRITINGRECORD_H
+
