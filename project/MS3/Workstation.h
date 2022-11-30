859c49563b3b8a7084142a9fc0f306d12342de1b
Carmen W <Carmenwhitton@gmail.com>
2022-11-29 23:48:57 -0500

    Set up Workstation header

--- project/MS3/Workstation.h
+++ project/MS3/Workstation.h
@@ -9,9 +9,28 @@
 #ifndef SDDS_WORKSTATION_H
 #define SDDS_WORKSTATION_H
 
-namespace sdds {
-   class Workstation {
+#include <deque>
+#include <string>
+#include "CustomerOrder.h"
+#include "Station.h"
 
+namespace sdds {
+
+   extern std::deque<CustomerOrder> g_pending;
+   extern std::deque<CustomerOrder> g_completed;
+   extern std::deque<CustomerOrder> g_incomplete;
+
+   class Workstation : public Station{
+      std::deque<CustomerOrder> m_orders{};
+      Workstation* m_pNextStation{};
+   public:
+      Workstation(const std::string&);
+      void fill(std::ostream& os);
+      bool attemptToMoveOrder();
+      void setNextStation(Workstation* station);
+      Workstation* getNextStation() const;
+      void display(std::ostream& os) const;
+      Workstation& operator+=(CustomerOrder&& newOrder);
    };
 }
 
