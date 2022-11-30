5ea7e70e062212b0ef28b068a1c340f24de6a5ea
Carmen W <Carmenwhitton@gmail.com>
2022-11-30 00:14:56 -0500

    Initial implementation of Workstation functions

--- project/MS3/Workstation.cpp
+++ project/MS3/Workstation.cpp
@@ -9,5 +9,64 @@
 #include "Workstation.h"
 
 namespace sdds {
+   // Parameterized constructor, just passes the string to the base class constructor
+   Workstation::Workstation(const std::string& file) : Station{file} {
 
+   }
+
+   // Fills the order at the front of the queue
+   void Workstation::fill(std::ostream& os) {
+      if (m_orders.size()) {
+         m_orders.front().fillItem(*this, os);
+      }
+   }
+
+   // Attempts to move the front order to the next station
+   bool Workstation::attemptToMoveOrder() {
+      bool moved = false;
+      // Move item if out of inventory, or
+      // TODO: DOUBLE CHECK THIS ASSUMPTION
+      // if there are no more items of the type this station carries to be filled
+      if (!Station::getQuantity() || m_orders.front().isItemFilled(Station::getItemName())) {
+         moved = true;
+         if (m_pNextStation) {
+            *m_pNextStation += std::move(m_orders.front());
+            // TODO: Not sure if this extra pop is necessary, check if it's double popping
+            m_orders.pop_front();
+         }
+         else if(m_orders.front().isOrderFilled()){
+            g_completed.push_back(std::move(m_orders.front()));
+            m_orders.pop_front();
+         }
+         else {
+            g_incomplete.push_back(std::move(m_orders.front()));
+            m_orders.pop_front();
+         }
+      }
+
+      return moved;
+   }
+
+   void Workstation::setNextStation(Workstation* station = nullptr) {
+      m_pNextStation = station;
+   }
+
+   Workstation* Workstation::getNextStation() const {
+      return m_pNextStation;
+   }
+
+   void Workstation::display(std::ostream& os) const {
+      std::cout << Station::getItemName() << " --> ";
+      if (m_pNextStation) {
+         std::cout << ((Station*)m_pNextStation)->getItemName();
+      }
+      else {
+         std::cout << "End of Line";
+      }
+      std::cout << "\n";
+   }
+
+   Workstation& Workstation::operator+=(CustomerOrder&& newOrder) {
+      m_orders.push_back(std::move(newOrder));
+   }
 }
